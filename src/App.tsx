b66e8432edcbdc0645fcc97ad57672b4cdad7103
parsTroy <troyparsons@hotmail.com>
2023-12-20 12:27:47 -0500

    Initial Commit Project Homepage

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './globals.css'
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+    return (
+        <main className='flex h-screen '>
+
+        </main>
+    )
+}
+
+export default App
