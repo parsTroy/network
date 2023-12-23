f7e0af9648d163eb52a32c8896e1f3bc7bb19140
parsTroy <troyparsons@hotmail.com>
2023-12-23 17:37:35 -0500

    Update mutations

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -69,22 +69,21 @@ export const useLikePost = () => {
 
 export const useSavePost = () => {
     const queryClient = useQueryClient();
+
     return useMutation({
-        mutationFn: ({ userId, postId }: { userId: string; postId: string }) =>
-            savePost(userId, postId),
-        onSuccess: () => {
+        mutationFn: ({ postId, userId }: { postId: string, userId: string }) => savePost(postId, userId), onSuccess: () => {
             queryClient.invalidateQueries({
-                queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
-            });
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
             queryClient.invalidateQueries({
-                queryKey: [QUERY_KEYS.GET_POSTS],
-            });
+                queryKey: [QUERY_KEYS.GET_POSTS]
+            })
             queryClient.invalidateQueries({
-                queryKey: [QUERY_KEYS.GET_CURRENT_USER],
-            });
-        },
-    });
-};
+                queryKey: [QUERY_KEYS.GET_CURRENT_USER]
+            })
+        }
+    })
+}
 
 export const useDeleteSavedPost = () => {
     const queryClient = useQueryClient();
