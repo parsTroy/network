e3543c9b0ff70ccbfda16d86a8451fa1963d8d40
parsTroy <troyparsons@hotmail.com>
2023-12-23 16:14:51 -0500

    Update signin form

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -54,7 +54,7 @@ const SigninForm = () => {
     return (
         <Form {...form}>
             <div className="sm:w-420 flex-center flex-col">
-                <img src="/assets/images/logo.svg" alt="logo" />
+                <img src="/assets/images/logo.png" alt="logo" />
 
                 <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
                     Log in to your account
