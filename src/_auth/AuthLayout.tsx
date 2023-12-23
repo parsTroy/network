fa1fd90a60f369e88e1ac207c4bee2400bb8bfed
parsTroy <troyparsons@hotmail.com>
2023-12-23 16:13:16 -0500

    Update assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -13,7 +13,7 @@ const AuthLayout = () => {
                     <section className="flex flex-1 justify-center items-center flex-col py-10">
                         <Outlet />
                     </section>
-                    <img src="/assets/images/side-img.svg" alt="logo" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
+                    <img src="/assets/images/side-img.jpg" alt="logo" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" />
                 </>
             )}
         </>
