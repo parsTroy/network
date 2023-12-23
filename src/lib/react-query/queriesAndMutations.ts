e6ea2587932f5454d194ce8160c522db511c5ce6
parsTroy <troyparsons@hotmail.com>
2023-12-23 17:45:25 -0500

    Update queries

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -146,8 +146,8 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
     return useInfiniteQuery({
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-        queryFn: getInfinitePosts as any,
-        getNextPageParam: (lastPage: any) => {
+        queryFn: getInfinitePosts,
+        getNextPageParam: (lastPage) => {
             if (lastPage && lastPage.documents.length === 0) return null;
 
             const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
