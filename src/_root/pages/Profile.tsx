32ce245d9ac38af53597ea65bb75906282e3438d
parsTroy <troyparsons@hotmail.com>
2023-12-24 11:03:33 -0500

    Update statblock on profile

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -61,11 +61,11 @@ const Profile = () => {
                             </p>
                         </div>
 
-                        <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
+                        {/* <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
                             <StatBlock value={currentUser.posts.length} label="Posts" />
                             <StatBlock value={20} label="Followers" />
                             <StatBlock value={20} label="Following" />
-                        </div>
+                        </div> */}
 
                         <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
                             {currentUser.bio}
