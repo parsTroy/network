12445681f6fb79f0f8ab28d1e7b0d59a9febbab4
parsTroy <troyparsons@hotmail.com>
2023-12-23 19:51:31 -0500

    Update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/network/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
