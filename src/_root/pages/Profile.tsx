cc5f9fae741cfbe2abd9263ee5e441461ef7c34a
parsTroy <troyparsons@hotmail.com>
2023-12-24 11:06:47 -0500

    Update stats block add posts

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -14,17 +14,17 @@ import GridPostList from "@/components/shared/GridPostList";
 import { LikedPosts } from ".";
 import { useUserContext } from "@/context/AuthContext";
 
-// type StatBlockProps = {
-//     value: string | number;
-//     label: string;
-// }
+type StatBlockProps = {
+    value: string | number;
+    label: string;
+}
 
-// const StatBlock = ({ value, label }: StatBlockProps) => (
-//     <div className="flex-center gap-2">
-//         <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
-//         <p className="small-medium lg:base-medium text-light-2">{label}</p>
-//     </div>
-// );
+const StatBlock = ({ value, label }: StatBlockProps) => (
+    <div className="flex-center gap-2">
+        <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
+        <p className="small-medium lg:base-medium text-light-2">{label}</p>
+    </div>
+);
 
 const Profile = () => {
     const { id } = useParams();
@@ -61,11 +61,11 @@ const Profile = () => {
                             </p>
                         </div>
 
-                        {/* <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
+                        <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
                             <StatBlock value={currentUser.posts.length} label="Posts" />
-                            <StatBlock value={20} label="Followers" />
-                            <StatBlock value={20} label="Following" />
-                        </div> */}
+                            {/* <StatBlock value={20} label="Followers" />
+                            <StatBlock value={20} label="Following" /> */}
+                        </div>
 
                         <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
                             {currentUser.bio}
