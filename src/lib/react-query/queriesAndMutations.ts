99db7060b655f13707b4f42a1434532ee1088fef
parsTroy <troyparsons@hotmail.com>
2023-12-23 19:22:12 -0500

    Debug queries

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -146,19 +146,16 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
     return useInfiniteQuery({
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-        queryFn: getInfinitePosts as any,
-        getNextPageParam: (lastPage: any) => {
-            // If there's no data, there are no more pages.
-            if (lastPage && lastPage.documents.length === 0) {
-                return null;
-            }
+        queryFn: getInfinitePosts,
+        getNextPageParam: (lastPage) => {
+            if (lastPage && lastPage.documents.length === 0) return null;
 
-            // Use the $id of the last document as the cursor.
-            const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+            const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
+
             return lastId;
-        },
-    });
-};
+        }
+    })
+}
 
 export const useSearchPosts = (searchTerm: string) => {
     return useQuery({
